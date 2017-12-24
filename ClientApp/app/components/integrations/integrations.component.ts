d3d05360700e2320ad3168bcf27c6cfe3d7e5c48
Cumhur Düzlü <cumhurduzlu@Cumhurs-MacBook-Pro.local>
2017-12-24 19:07:04 +0100

    Spotify integration.

--- ClientApp/app/components/integrations/integrations.component.ts
+++ ClientApp/app/components/integrations/integrations.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    templateUrl: './integrations.component.html'
+    selector: 'integrations',
+    templateUrl: './integrations.component.html',
+    styleUrls: [ './integrations.component.css' ]
 })
-export class IntegrationsComponent {
-    public currentCount = 0;
+export class IntegrationsComponent implements OnInit{
+    token: string;
 
-    public incrementCounter() {
-        this.currentCount++;
+    constructor(private route: ActivatedRoute) {
+        
+    }
+
+    ngOnInit(): void {
+        this.route.queryParams.subscribe(queryParams => {
+            if (queryParams && 
+                queryParams.source == 'spotify')
+                this.token = queryParams.code;
+        });
+    }
+
+    public connectSpotify(){
+        let spotifyUri = 'https://accounts.spotify.com/authorize?';
+        let clientId = '36fdb6cef7e54969829637ca90d0fe0d';
+        let responseType = 'code';
+        let redirectUri = 'http://localhost:5000/integrations?source=spotify';
+
+        window.location.href = spotifyUri +
+        'client_id=' + clientId + '&' +
+        'response_type=' + responseType + '&' +
+        'redirect_uri=' + redirectUri;
     }
 }
